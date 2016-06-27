845b04a6e608f34d738c94406c9c1547dd685e2e
berkowitze <elias_berkowitz@brown.edu>
2016-06-26 21:10:08 -0500

    tiny updates

--- Scripts/variables.js
+++ Scripts/variables.js
@@ -1,3 +1,4 @@
+// Constants
 var msPerYear = 3.15569e10;
 var msPerMonth = 2.62974e9;
 var msPerWeek = 604800000;
@@ -8,38 +9,22 @@ var msPerSecond = 1000;
 var minutesPerYear = 525949;
 var secPerDay = 86400;
 var averageDaysPerMonth = 30.4375;
-var msInHour;
-var gender;
-var countryValue;
-var almostDead;
-var aboveLE;
-var ageRange;
-var slope;
-var yIntercept;
-var lifeExpectancy;
-var lE;
-var msPerLife;
-var percentLifePassed;
-var moreInfo;
+
+// Messages
 var dOBError = "Please enter your birth country, date of birth and gender";
 var almostDead = "You're in your last moments";
 var invalidDOB = "You're not alive... please enter a <i>real</i> date of birth";
 var aboveLE = "You lived over your average life expectancy - Congratulations!";
-var nextPercent;
-var pUntilNextPercent;
-var tUntilNextPercent;
-var unitToUseForDeriv;
-var derivPercent;
-var leapYear;
-var daysInMonth;
-var arrayToUse;
-var previousMonth;
-var yob;
-var mob;
-var dob;
-var realYearsAlive;
-var arrayTwo = [[" years", " year"], [" months", " month"], [" days"," day"], [" hours", " hour"], [" minutes", " minute"], [" seconds", " second"]]
-var daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-var saved;
-var monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June',
-'July', 'August', 'September', 'October', 'November', 'December']
+
+var monthsOfYear = ['January', 'February', 'March',
+					'April', 'May', 'June', 'July',
+					'August', 'September', 'October',
+					'November', 'December'];
+
+formDict = {
+	'yearofbirth': undefined,
+	'monthofbirth': undefined,
+	'dayofbirth': undefined,
+	'sex': undefined,
+	'country': undefined
+}
